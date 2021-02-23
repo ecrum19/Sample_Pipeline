88bea226585c32e0c345b96ac133f43e71870552
Elias D. Crum <ecrum@luc.edu>
2021-02-23 16:31:07 -0600

    Python file containing methods that execute parts of the mini_project. This version contains a function using wget to download the transcriptome files.

---
+++ mini_proj.py
@@ -0,0 +1,30 @@
+import os
+
+
+def get_seqs(seq_addresses):        # downloads seqs via SRR address
+    for i in seq_addresses:
+        os.system("wget " + i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Driver code
+transcript_seqs = ['https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660030/SRR5660030.1',
+                   'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660033/SRR5660033.1',
+                   'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660044/SRR5660044.1',
+                   'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660045/SRR5660045.1']
+
+get_seqs(transcript_seqs)
