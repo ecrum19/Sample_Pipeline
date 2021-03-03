9a1a3d59059d65cf7b45acb6c1ad0ed93eaa6c28
ecrum19 <49316983+ecrum19@users.noreply.github.com>
2021-03-03 10:10:08 -0600

    Removed # from commands

--- mini_proj_wrapper.py
+++ mini_proj_wrapper.py
@@ -14,17 +14,17 @@ def test_or_full():
     args = run.parse_args()
     return vars(args)['t']
 
-#os.system('cd ~/mini_proj/')
-#os.system('python3 pull_files.py')      # pulls transcriptome + reference CDS map data
-#os.system('python3 split_fastq.py')     # splits fastq files and cleans directory
-#os.system('python3 run_kallisto.py')    # analyzes transcriptome data using kallisto
-#os.system('python3 sleuth_tab.py')      # produces table for sleuth
-#os.system('python3 sleuth_write.py')    # writes sleuth output to .log
-#os.system('python3 bowtie.py')          # runs bowtie seq mapping
-#os.system('python3 bowtie_charact.py')  # writes number of reads before and after mapping to .log
-#os.system('python3 spades.py')          # assemples a de novo genome from the mapped bowtie transcriptome reads
-#os.system('python3 contig_len.py')      # determines the number of contigs >1000 bp obtained from spades assembly
-#os.system('python3 assembly_len.py')    # determines the length of assembled genome with >1000 bp scaffolds
-#os.system('python3 blast.py')           # runs local BLAST with longest scaffold from assembly, writes outputs to .log
+os.system('cd ~/mini_proj/')
+os.system('python3 pull_files.py')      # pulls transcriptome + reference CDS map data
+os.system('python3 split_fastq.py')     # splits fastq files and cleans directory
+os.system('python3 run_kallisto.py')    # analyzes transcriptome data using kallisto
+os.system('python3 sleuth_tab.py')      # produces table for sleuth
+os.system('python3 sleuth_write.py')    # writes sleuth output to .log
+os.system('python3 bowtie.py')          # runs bowtie seq mapping
+os.system('python3 bowtie_charact.py')  # writes number of reads before and after mapping to .log
+os.system('python3 spades.py')          # assemples a de novo genome from the mapped bowtie transcriptome reads
+os.system('python3 contig_len.py')      # determines the number of contigs >1000 bp obtained from spades assembly
+os.system('python3 assembly_len.py')    # determines the length of assembled genome with >1000 bp scaffolds
+os.system('python3 blast.py')           # runs local BLAST with longest scaffold from assembly, writes outputs to .log
 
 
